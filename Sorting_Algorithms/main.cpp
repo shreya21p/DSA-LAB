1d5ce3e07ca2aca56608cd3721445ab6a8419c56
shreya21p <021sods@gmail.com>
2025-05-01 19:08:24 +0530

    Update main.cpp

--- Sorting_Algorithms/main.cpp
+++ Sorting_Algorithms/main.cpp
@@ -1,4 +1,4 @@
-
+//Program to implement bubblesort, selection sort and insertion sort
 #include<cstdlib>
 #include<cstdio>
 #include "sort.h"
@@ -8,6 +8,8 @@ void insertionsort(int *arr,int size);
 void displayarray(int *arr,int size);
 int main(){
     int size,choice;
+    
+   
     int cond=1;
     while(cond){
         printf("\n1.Bubblesort\n");
@@ -17,36 +19,86 @@ int main(){
         printf("Please enter your choice:");
         scanf("%d",&choice);
         if(choice!=4){
-            printf("enter the number of elements:");
-            scanf("%d",&size);
-            int *arr=(int*)malloc(size*sizeof(int));
-            printf("Enter the elements:");
-            for(int i=0;i<size;i++){
-                scanf("%d",&arr[i]);
-            }
-            switch(choice){
-                case 1:
-                    bubblesort(arr,size);
-                    printf("Array after bubblesort:");
-                    displayarray(arr,size);
-                    break;
-                case 2:
-                    selectionsort(arr,size);
-                    printf("Array after selection sort:");
-                    displayarray(arr,size);
-                    break;
-                case 3:
-                    insertionsort(arr,size);
-                    printf("Array after insertion sort:");
-                    displayarray(arr,size);
-                    break;
-                default:
-                    printf("invalid choice, please try\n");
-            }
-        } else {
-            printf("Exiting the program");
-            break;
+
+    
+        printf("enter the number of elements:");
+        scanf("%d",&size);
+        int *arr=(int*)malloc(size*sizeof(int));
+        printf("Enter the elements:");
+        for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+        switch(choice){
+            case 1:
+            
+                bubblesort(arr,size);
+                printf("Array after bubblesort:");
+                displayarray(arr,size);
+                break;
+	case 2:
+                selectionsort(arr,size);
+                printf("Array after selection sort:");
+                displayarray(arr,size);
+                break;
+	        case 3:
+                insertionsort(arr,size);
+                printf("Array after insertion sort:");
+                displayarray(arr,size);
+                break;
+            default:
+                printf("invalid choice, please try\n");
+
         }
     }
-    return 0;
+    else{
+        printf("Exiting the program");
+        break;}
+    
+}return 0;
+}
+
+//Sorts numbers using bubblesort 
+void bubblesort(int *arr, int size){    for (int i=0;i<size-1;i++){
+        for (int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+//Sorts using selectionsort
+void selectionsort(int *arr,int size){ 
+    for (int i=0;i<size-1;i++){
+        int minindex=i;
+        for (int j=i+1;j<size;j++){
+            if(arr[j]<arr[minindex]){
+                minindex=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[minindex];
+        arr[minindex]=temp;
+    }
+}
+
+//Sorts using insertionsort
+void insertionsort(int *arr,int size){ 
+    for(int i=1;i<size;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+void displayarray(int *arr,int size){
+    for (int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
 }
