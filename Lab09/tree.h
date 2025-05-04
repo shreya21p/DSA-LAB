499048bae1304dd7e55cd2038d4dd1b899bed1ba
shreya21p <021sods@gmail.com>
2025-05-05 01:59:03 +0530

    Update tree.h

--- Lab09/tree.h
+++ Lab09/tree.h
@@ -1,8 +1,11 @@
-HEADER FILE:
-//Header file for BST expression tree
+// Header file for BST expression tree 
+#ifndef BST_H
 #define BST_H
 
-#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <cctype>
+#include <string>
 using namespace std;
 
 class BST {
@@ -13,8 +16,7 @@ public:
         TreeNode* right;
         TreeNode(char val) : value(val), left(NULL), right(NULL) {}
     };
-    
-    
+
     TreeNode* root;
 
     BST() { root = nullptr; }
@@ -28,41 +30,44 @@ public:
 // Preorder traversal (Root-Left-Right)
 void BST::preorder(TreeNode* root) {
     if (root != nullptr) {
-        cout << root→value << " ";
-        preorder(root→left);
-        preorder(root→right);
+        printf("%c ", root->value);
+        preorder(root->left);
+        preorder(root->right);
     }
 }
+
 // Inorder traversal (Left-Root-Right)
 void BST::inorder(TreeNode* root) {
     if (root != nullptr) {
-        inorder(root→left);
-        cout << root→value << " ";
-        inorder(root→right);
+        inorder(root->left);
+        printf("%c ", root->value);
+        inorder(root->right);
     }
 }
 
 // Postorder traversal (Left-Right-Root)
 void BST::postorder(TreeNode* root) {
     if (root != nullptr) {
-        postorder(root→left);
-        postorder(root→right);
-        cout << root→value << " ";
+        postorder(root->left);
+        postorder(root->right);
+        printf("%c ", root->value);
     }
 }
 
-// Constructing tree
+// Construct expression tree from postfix
 void BST::constructTree(string postfix) {
     stack<TreeNode*> st;
     for (char ch : postfix) {
         if (isalnum(ch)) {
             st.push(new TreeNode(ch));
-        } else { 
+        } else {
             TreeNode* node = new TreeNode(ch);
-            node→right = st.top(); st.pop();
-            node→left = st.top(); st.pop();
+            node->right = st.top(); st.pop();
+            node->left = st.top(); st.pop();
             st.push(node);
         }
     }
     root = st.top();
 }
+
+#endif
